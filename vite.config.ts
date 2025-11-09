fca797cc518632c176b230fc70862b2f07183946
Pete Allen <pete@cleartriage.com>
2025-11-08 22:28:52 -0700

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  appType: 'spa'
+  appType: 'spa',
+  // Required so asset URLs resolve correctly when served from https://peteallen.github.io/chickengame/
+  base: '/chickengame/'
 });
